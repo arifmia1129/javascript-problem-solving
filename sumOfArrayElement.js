2c88ed1551a97e7f082f3abd4bba087e6f7a2aa1
arifvtti <arif.vtti@gmail.com>
2023-03-15 08:14:11 +0600

    sum of array elements

---
+++ sumOfArrayElement.js
@@ -0,0 +1,11 @@
+// Coding challenge #10: Calculate the sum of numbers in an array of numbers
+
+const arr = [1, 2, 3];
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+console.log(sum);

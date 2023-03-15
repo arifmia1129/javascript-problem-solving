7fe59eb3313673258340bf2f412a61bbcaaa696f
arifvtti <arif.vtti@gmail.com>
2023-03-15 10:15:09 +0600

    reverse string

---
+++ reverseString.js
@@ -0,0 +1,13 @@
+// Reverse a string
+
+function reverseString(string) {
+    let newString = "";
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        newString += string[i];
+    }
+
+    return newString;
+}
+
+console.log(reverseString("Javascript"));

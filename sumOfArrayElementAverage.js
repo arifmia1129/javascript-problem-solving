2502b55263d9a974a08657fd6c8fb803624cc546
arifvtti <arif.vtti@gmail.com>
2023-03-15 08:22:27 +0600

    array elements sum average

---
+++ sumOfArrayElementAverage.js
@@ -0,0 +1,14 @@
+// Coding challenge #11: Calculate the average of the numbers in an array of numbers
+
+const arr = [1, 2, 3];
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+
+const average = sum / arr.length;
+
+console.log(average);
+

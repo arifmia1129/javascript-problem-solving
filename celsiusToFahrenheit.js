753b1b86108a773a8ae81833af8fa6d371cc1921
arifvtti <arif.vtti@gmail.com>
2023-03-15 08:01:13 +0600

    celsius to fahrenheit

---
+++ celsiusToFahrenheit.js
@@ -0,0 +1,9 @@
+// Coding challenge #8: Create a function that will convert from Celsius to Fahrenheit
+
+function celsiusToFahrenheit(celsius) {
+    const fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+    console.log(`Fahrenheit temperature: ${fahrenheit}`);
+}
+
+celsiusToFahrenheit(20);
